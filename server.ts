807c9a0cd60bba693f5d84e6a2487f2f9c3c967c
codecycleteam <84431724+codecycleteam@users.noreply.github.com>
2022-04-06 08:03:08 +0000

    added deno server

--- server.ts
+++ server.ts
@@ -1,16 +1,35 @@
-import { serve } from "https://deno.land/std/http/server.ts";
-import { refresh } from "https://deno.land/x/refresh/mod.ts";
+// import { serve } from "https://deno.land/std/http/server.ts";
+// import { refresh } from "https://deno.land/x/refresh/mod.ts";
 
-// Create refresh middleware
-const middleware = refresh();
+// // Create refresh middleware
+// const middleware = refresh();
 
-serve( async ( req ) =>
+// serve( async ( req ) =>
+// {
+//     // In your server handler, just add into the middleware stack!
+//     const res = middleware( req );
+
+//     if ( res ) return res;
+//     const hello = await Deno.readFile( './index.html' );
+
+//     return new Response( hello, { status: 200, headers: { "Content-type": "text/html" } } );
+// } );
+
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use( async ( context, next ) =>
 {
-    // In your server handler, just add into the middleware stack!
-    const res = middleware( req );
-
-    if ( res ) return res;
-    const hello = await Deno.readFile( './index.html' );
-
-    return new Response( hello, { status: 200, headers: { "Content-type": "text/html" } } );
+    try
+    {
+        await context.send( {
+            root: `${ Deno.cwd() }/`,
+            index: "index.html",
+        } );
+    } catch {
+        next();
+    }
 } );
+
+await app.listen( { port: 8000 } );
