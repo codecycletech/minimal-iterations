e4b77c6c8540e0121b3700133c8b00530c5f817c
codecycleteam <84431724+codecycleteam@users.noreply.github.com>
2022-04-06 08:05:40 +0000

    removed comment

--- server.ts
+++ server.ts
@@ -1,20 +1,3 @@
-// import { serve } from "https://deno.land/std/http/server.ts";
-// import { refresh } from "https://deno.land/x/refresh/mod.ts";
-
-// // Create refresh middleware
-// const middleware = refresh();
-
-// serve( async ( req ) =>
-// {
-//     // In your server handler, just add into the middleware stack!
-//     const res = middleware( req );
-
-//     if ( res ) return res;
-//     const hello = await Deno.readFile( './index.html' );
-
-//     return new Response( hello, { status: 200, headers: { "Content-type": "text/html" } } );
-// } );
-
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
